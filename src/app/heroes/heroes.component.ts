de6a3f5c259cb49770fc3eb6ff740878cd0234fb
jari.annala <jari.annala@siili.com>
2020-02-20 12:38:50 +0200

    hero stuff

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 
 
@@ -9,10 +10,12 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
   constructor() { }
 
